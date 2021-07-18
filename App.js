20b9aa4ad3b17eb80baec66ea3a99c3855e57f55
IR~SAGE <sibo.egide98@gmail.com>
2021-07-18 08:22:15 +0200

    Working with StyleSheet Styling

--- App.js
+++ App.js
@@ -3,23 +3,11 @@ import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ padding: 50 }}>
+    <View style={styles.screen}>
       <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
-        }}
+        style={styles.inputContainer}
       >
-        <TextInput
-          placeholder="Course Goal"
-          style={{
-            width: "80%",
-            borderColor: "black",
-            borderWidth: 1,
-            padding: 10,
-          }}
-        />
+        <TextInput placeholder="Course Goal" style={styles.inputText} />
         <Button title="ADD" />
       </View>
     </View>
@@ -27,4 +15,18 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
+  screen: {
+    padding: 50,
+  },
+  inputText: {
+    width: "80%",
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
 });
