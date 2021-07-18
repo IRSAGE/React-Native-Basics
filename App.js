30bb73528a9c2068d5db3234420074548136400c
IR~SAGE <sibo.egide98@gmail.com>
2021-07-18 10:01:25 +0200

    Working With Touchables And Delete An Item

--- App.js
+++ App.js
@@ -1,34 +1,38 @@
 import React, { useState } from "react";
-import {
-  FlatList,
-  StyleSheet,
-  View,
-} from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
 import GoalInput from "./components/GoalInput";
 import GoalItem from "./components/GoalItem";
 
 export default function App() {
-  
   const [courseGoals, setCourseGoals] = useState([]);
 
   const addGoalHandler = (goalTitle) => {
     setCourseGoals((currentGoals) => [
       ...currentGoals,
-      { key: Math.random().toString(), value: goalTitle },
+      { id: Math.random().toString(), value: goalTitle },
       // you can use key or id both works
     ]);
   };
 
+  const removeGoalHandler = (goalId) => {
+    setCourseGoals((currentGoals) => {
+      return currentGoals.filter((goal) => goal.id !== goalId);
+    });
+  };
+
   return (
     <View style={styles.screen}>
-      <GoalInput onAddGoal={addGoalHandler}/>
-        <FlatList
-          data={courseGoals}
-          renderItem={(itemData) => {
-            <GoalItem title={itemData.item.value} />;
-          }}
-        />
-  
+      <GoalInput onAddGoal={addGoalHandler} />
+      <FlatList
+        data={courseGoals}
+        renderItem={(itemData) => {
+          <GoalItem
+            id={itemData.id}
+            onDelete={removeGoalHandler}
+            title={itemData.item.value}
+          />;
+        }}
+      />
     </View>
   );
 }
